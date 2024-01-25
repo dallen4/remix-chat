5fcd623e286c4ebb3ed74f960c4d081d134e6fcc
Nieky <nieky.allen@slalom.com>
2024-01-24 21:50:53 -0600

    setup work project

--- worker/Session.ts
+++ worker/Session.ts
@@ -1,10 +1,28 @@
 import { Env } from './index';
+import { serialize } from 'cookie';
+
+interface Message {
+  timestamp: number;
+  body: string;
+}
 
 export class Session {
   private _active: boolean;
+  private _connections: WebSocket[];
+  private _connMap: Map<string, WebSocket>;
+
+  private controller: DurableObjectState;
+  private store: DurableObjectStorage;
+  private env: Env;
 
   constructor(controller: DurableObjectState, env: Env) {
     this._active = false;
+    this._connections = [];
+    this._connMap = new Map<string, WebSocket>();
+
+    this.controller = controller;
+    this.store = this.controller.storage;
+    this.env = env;
   }
 
   get isActive() {
@@ -14,8 +32,69 @@ export class Session {
   async fetch(
     request: Request<unknown, CfProperties<unknown>>
   ): Promise<Response> {
-    return new Response();
+    if (!this._active && request.headers.get('Upgrade') !== 'websocket')
+      return new Response('Expected websocket', { status: 400 });
+
+    const headers = new Headers();
+
+    const sessionId = this.controller.id.toString();
+
+    headers.set('Set-Cookie', serialize('session_id', sessionId));
+    headers.set('x-session-id', sessionId);
+
+    if (request.method === 'PUT') {
+      this.broadcast({
+        timestamp: Date.now(),
+        body: await request.text(),
+      });
+
+      return new Response('message sent', { headers });
+    }
+
+    const client = await this.initWebsocket();
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+      headers,
+    });
   }
 
-  async createConnection() {}
+  private async initWebsocket() {
+    const [client, server] = Object.values(new WebSocketPair());
+
+    server.accept();
+
+    server.addEventListener('message', this.handleMessage);
+
+    server.addEventListener('error', (err) => {
+      console.error(err);
+      this.broadcast({ timestamp: Date.now(), body: 'error occurred' });
+    });
+
+    server.addEventListener('close', (event) => {
+      console.log('Connection closed: ', event);
+    });
+
+    this._connections.push(server);
+
+    return client;
+  }
+
+  private handleMessage({ type, data: rawData }: MessageEvent) {
+    const data = JSON.parse(rawData as string);
+
+    console.log(data);
+
+    if (type === 'error') {
+    } else if (type === 'message') {
+    } else {
+    }
+  }
+
+  private broadcast(msg: Message) {
+    const msgAsString = JSON.stringify(msg);
+
+    for (const conn of this._connections) conn.send(msgAsString);
+  }
 }
--- worker/index.ts
+++ worker/index.ts
@@ -1,65 +1,37 @@
-import template from './template';
+import { parse } from 'cookie';
 import { Session } from './Session';
-
-let count = 0;
 
 export interface Env {
   sessions: DurableObjectNamespace;
   WORKER_URI: string;
 }
 
-async function handleSession(websocket: WebSocket) {
-  websocket.accept();
-  websocket.addEventListener('message', async ({ data }) => {
-    if (data === 'CLICK') {
-      count += 1;
-      websocket.send(JSON.stringify({ count, tz: new Date() }));
-    } else {
-      // An unknown message came into the server. Send back an error message
-      websocket.send(
-        JSON.stringify({ error: 'Unknown message received', tz: new Date() })
-      );
-    }
-  });
-
-  websocket.addEventListener('close', async (evt) => {
-    // Handle when a client closes the WebSocket connection
-    console.log(evt);
-  });
-}
-
-const websocketHandler = async (request: Request) => {
-  const upgradeHeader = request.headers.get('Upgrade');
-  if (upgradeHeader !== 'websocket') {
-    return new Response('Expected websocket', { status: 400 });
-  }
-
-  const [client, server] = Object.values(new WebSocketPair());
-  await handleSession(server);
-
-  return new Response(null, {
-    status: 101,
-    webSocket: client,
-  });
-};
-
 async function handleRequest(
   request: Request,
   env: Env,
   ctx: ExecutionContext
 ) {
-  const sessionId = request.headers.get('x-session-id');
-  const sessionObjectId = env.sessions.idFromString(sessionId);
+  function getId() {
+    const cookie = parse(request.headers.get('Cookie') || '');
 
-  const session = env.sessions.get(sessionObjectId) as unknown as Session;
+    const sessionId =
+      cookie['session_id'] || request.headers.get('x-session-id');
+
+    return sessionId
+      ? env.sessions.idFromString(sessionId)
+      : env.sessions.newUniqueId();
+  }
+
+  const sessionObjectId = getId();
+  console.log(sessionObjectId.toString());
+  const session = env.sessions.get(sessionObjectId);
 
   try {
     const url = new URL(request.url);
+
     switch (url.pathname) {
       case '/':
-        return template();
-      case '/ws':
-        return websocketHandler(request);
+        return session.fetch(request);
       default:
         return new Response('Not found', { status: 404 });
     }
